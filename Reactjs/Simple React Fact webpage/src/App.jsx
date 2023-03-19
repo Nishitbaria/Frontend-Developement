82e048579ca8d031574b9b605aff0518eda43179
Nishitbaria <Nishitbaria@gmail.com>
2023-03-19 22:10:20 +0530

    Adding Reacjs Page✔

---
+++ Reactjs/Simple React Fact webpage/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './componets/Navbar.jsx'
+import Maincontent from './componets/Maincontent'
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar/>
+    <Maincontent/>
+    </>
+  )
+}
+
+export default App
---
+++ Reactjs/Simple React Fact webpage/src/componets/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default  function Navbar() {
+
+return ( 
+  <nav>
+            <img src="/react.svg" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+
+)
+
+}
